61870807eaef19cbca41950502208148a2d03aa9
GilbertBautista5 <Gmikhail24@gmail.com>
2023-10-05 23:54:29 +1100

    5/10

--- Frontend/Capstone/src/Pages/Homepage.jsx
+++ Frontend/Capstone/src/Pages/Homepage.jsx
@@ -6,15 +6,15 @@ export default function Homepage() {
   return (
     <div className="HomePage">
       <h1> Welcome to PlanAhead</h1>
-      <DrawerAppBar />
       <p className="Paragraph">
-        PlanAhead is the ultimate daily planning application, offering users a
+        " PlanAhead is the ultimate daily planning application, offering users a
         robust tool to optimize their daily routines and enhance productivity
         with user-friendly features, including customizable to-do lists,
         calendar integration, priority management, and real-time reminders. This
         app is your trusted companion for efficient time management and
-        achieving a more organized and balanced life.
+        achieving a more organized and balanced life. "
       </p>
+      <DrawerAppBar />
       <div className="SignUpContainer">
         {" "}
         <img src="../../public/Images/undraw.svg" alt="logo" height={500} width={500} />
